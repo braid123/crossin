eef26ea00f14bc1ec4de82aaadae0d4d8faef675
braid123 <603505420@qq.com>
2017-11-20 12:02:57 +0800

    git commit -m "init commit"

--- lab/group/urls.py
+++ lab/group/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.student_group, name='studentgroup'),
+    url(r'^$', views.student_group, name='group'),
     url(r'^student', views.student_group, name='studentgroup'),
 ]

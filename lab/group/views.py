fb1945b00cf6e824f14e4a7e57ad6a86fe2c8fb3
braid123 <603505420@qq.com>
2017-11-23 09:19:22 +0800

    init commit

--- lab/group/views.py
+++ lab/group/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 import labcrm.models
 from group import models
 
+
 @log_this
 @login_required
 def student_group(request):
@@ -35,8 +36,31 @@ def _student_group_get(request):
 def _student_group_post(request):
     groupname = request.POST.get('groupname')
     groupnote = request.POST.get('groupnote')
-    group, is_new = models.StudentGroup.objects.get_or_create(group_name=groupname,group_note=groupnote)
-    if is_new:
-        return HttpResponse(group.group_name,group.group_note)
+    if groupname and groupnote:
+        group, is_new = models.StudentGroup.objects.get_or_create(group_name=groupname,group_note=groupnote)
+        if is_new:
+            return HttpResponse(group.group_name, group.group_note)
+        else:
+            return HttpResponse()
     else:
-        return HttpResponse()
+        studentgroup = request.POST.getlist('studentgroup')
+        newgroup = request.POST.get('selectgroup')
+        newgrouptest = get_object_or_404(models.StudentGroup, id=newgroup)
+        students = labcrm.models.LabUser.objects.filter(id__in=studentgroup)
+        for student in students:
+            print('1111111',student.group.id)
+            student.group = newgrouptest
+            student.save()
+            print('群组用户名:', student)
+            print('原群组ID:', studentgroup)
+            print('新群组ID:', newgroup)
+            print('22222222222', student.group.id)
+        groups = models.StudentGroup.objects.all()
+        users = labcrm.models.LabUser.objects.filter(is_del=False).order_by('-user__date_joined')
+        group_id = request.GET.get('group', 1)
+        group = models.StudentGroup.objects.get(id=group_id)
+        return render(request, 'group/student.html', {
+            'users': users,
+            'groups': groups,
+            'group': group,
+        })
